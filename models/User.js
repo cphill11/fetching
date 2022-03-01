a40ac2915ba4d6d89cffbb78dc87ffc42a2fec19
Criste Phillips <criste.phillips@yahoo.com>
2022-03-01 17:48:08 -0500

    columns added to user-model

--- models/User.js
+++ models/User.js
@@ -1,12 +1,11 @@
 // import important parts of sequelize library
-const { Model, DataTypes } = require('sequelize');
+const { Model, DataTypes } = require("sequelize");
 // import our database connection from config.js
-const sequelize = require('../config/connection');
-// Initialize Product model (table) by extending off Sequelize's Model class
+const sequelize = require("../config/connection");
+// Initialize User model (table) by extending off Sequelize's Model class
 class User extends Model {}
 
-
-// set up fields and rules for Product model
+// create up fields and rules for User model
 User.init(
   {
     // define columns
@@ -14,41 +13,45 @@ User.init(
       type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      autoIncrement: true,
     },
-    product_name: {
+    user_name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
     },
-    // price: {
-    //   type: DataTypes.DECIMAL(10, 2),
-    //   allowNull: false,
-    //   validate: {
-    //     isDecimal: true
-    //   }
-    // },
-    // stock: {
-    //   type: DataTypes.INTEGER,
-    //   allowNull: false,
-    //   defaultValue: 10,
-    //   validate: {
-    //     isNumeric: true
-    //   }
-    // },
-    category_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'category',
-        key: 'id'
+    pet_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    comment_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    post_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'post',
+          key: 'id'
+        }
       }
-    }
-  },
+    },
+    // category_id: {
+    //   type: DataTypes.INTEGER,
+    //   references: {
+    //     model: "category",
+    //     key: "id",
+    //   },
+    // },
+
   {
     sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'user',
+    modelName: "user",
   }
 );
 
