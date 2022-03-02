4426d3a00fe92b02b2a5806be2eb67e90d69be6c
Criste Phillips <criste.phillips@yahoo.com>
2022-03-01 20:43:37 -0500

    env file added

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const profileRoutes = require('./profile-routes');
+const userRoutes = require('./user-routes');
 
 router.use('/profiles', profileRoutes);
+router.use('/user', userRoutes);
 
 
 module.exports = router;
