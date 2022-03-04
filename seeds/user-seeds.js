cd9cc68039eec8a754c3004d99022f91cb5a1c02
Criste Phillips <criste.phillips@yahoo.com>
2022-03-03 20:48:37 -0500

    user-seeds updated

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,35 +3,35 @@ const { User } = require('../models');
 const userData = [
   {
     user_name: 'Criste Phillips',
-    pet_name: 'Ripley',
+    // pet_name: 'Ripley',
     comment_text: '',
     email: 'a@a.com',
     password: '12345678'
   },
   {
     user_name: 'Dariga Iskakova',
-    pet_name: 'Patrick',
+    // pet_name: 'Patrick',
     comment_text: '',
     email: 'b@a.com',
     password: '12345678'
   },
   {
     user_name: 'Dariga Iskakova',
-    pet_name: 'Baby Birkin',
+    // pet_name: 'Baby Birkin',
     comment_text: '',
     email: 'c@a.com',
     password: '12345678'
   },
   {
     user_name: 'Danielle Fortin',
-    pet_name: 'Chance',
+    // pet_name: 'Chance',
     comment_text: '',
     email: 'd@a.com',
     password: '12345678'
   },
   {
     user_name: 'Michael Monihan',
-    pet_name: 'Lexi',
+    // pet_name: 'Lexi',
     comment_text: '',
     email: 'e@a.coms',
     password: '12345678'
