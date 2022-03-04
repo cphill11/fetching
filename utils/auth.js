b45b8ef26afce2ace8930e52c72a1a06d77451a4
Criste Phillips <criste.phillips@yahoo.com>
2022-03-03 19:17:58 -0500

    authorization js established

---
+++ utils/auth.js
@@ -0,0 +1,10 @@
+// fxn acts as a normal request check-back fxn, checks for existence of session property & then populates it w/ the fxn
+const withAuth = (req, res, next) => {
+    if (!req.session.loggedIn) {
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  };
+  
+  module.exports = withAuth;
