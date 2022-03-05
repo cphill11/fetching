17417ebb6b5cbd204046f24efdf0307be9e2ac97
Criste Phillips <criste.phillips@yahoo.com>
2022-03-05 15:19:49 -0500

    signup js added

---
+++ public/javascript/signup.js
@@ -0,0 +1,36 @@
+// listen for submit event in the form
+function signupFormHandler(event) {
+  event.preventDefault();
+}
+
+async function signupFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  // POST method for user interaction to go from form to our server
+  // use conditional to make sure all fields have values before making the POST request
+  if (username && email && password) {
+    const response = await fetch("/api/user", {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    // check the response status
+    if (response.ok) {
+      console.log("success");
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
