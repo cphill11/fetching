7f63e352850971ff0b87372a72ff553c5e490f7c
Criste Phillips <criste.phillips@yahoo.com>
2022-03-05 16:30:19 -0500

    notes updated

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,29 +1,28 @@
 // listen for the login event in the form
 async function loginFormHandler(event) {
-    event.preventDefault();
-  
-    const email = document.querySelector("#email-login").value.trim();
-    const password = document.querySelector("#password-login").value.trim();
-  
-    if (email && password) {
-      const response = await fetch("/api/user/login", {
-        method: "post",
-        body: JSON.stringify({
-          email,
-          password,
-        }),
-        headers: { "Content-Type": "application/json" },
-      });
-  
-      if (response.ok) {
-        document.location.replace("/pet");
-      } else {
-        alert(response.statusText);
-      }
+  event.preventDefault();
+
+  const email = document.querySelector("#email-login").value.trim();
+  const password = document.querySelector("#password-login").value.trim();
+
+  if (email && password) {
+    const response = await fetch("/api/user/login", {
+      method: "post",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/pet");
+    } else {
+      alert(response.statusText);
     }
   }
+}
 
-  
-  
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
-  
+document
+  .querySelector(".login-form")
+  .addEventListener("submit", loginFormHandler);
--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -1,17 +1,16 @@
-// cleanup to match our code
 // click event handler that calls the /logout route in main.handlebars
 
 async function logout() {
-    const response = await fetch('/api/user/logout', {
-      method: 'post',
-      headers: { 'Content-Type': 'application/json' }
-    });
-  
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert(response.statusText);
-    }
+  const response = await fetch("/api/user/logout", {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/");
+  } else {
+    alert(response.statusText);
   }
-  
-  document.querySelector('#logout').addEventListener('click', logout);
+}
+
+document.querySelector("#logout").addEventListener("click", logout);
