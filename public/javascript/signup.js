137205cc248a65befc5572a36b46c0c3cce849a3
Danielle Fortin <fortind6@gmail.com>
2022-03-06 16:40:58 -0500

    sign up js working

---
+++ public/javascript/signup.js
@@ -0,0 +1,34 @@
+// listen for submit event in the form
+function signupFormHandler(event) {
+  event.preventDefault();
+}
+
+async function signupFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+
+  // POST method for user interaction to go from form to our server
+  // use conditional to make sure all fields have values before making the POST request
+  if (username && email && password) {
+    const response = await fetch("/api/user", {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        email,
+        password,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+    // check the response status
+    if (response.ok) {
+      document.location.replace('/login');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
+document.querySelector(".signup-form").addEventListener("submit", signupFormHandler);
