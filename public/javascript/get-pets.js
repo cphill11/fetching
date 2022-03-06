aeb6bad2c924bdd1351679064feafb064634916d
Danielle Fortin <fortind6@gmail.com>
2022-03-06 18:19:57 -0500

    pet-card handelbars set up

---
+++ public/javascript/get-pets.js
@@ -0,0 +1,5 @@
+// this is where the code will go for the function to render pets to the page
+
+//when window loads
+
+//render pet
--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
+const helpers = require('./utils/helper');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ });
+const hbs = exphbs.create({ helpers });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
