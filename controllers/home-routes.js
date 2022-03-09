7a73324bcfdb734dd6e204143630f40c8d80a58c
Danielle Fortin <fortind6@gmail.com>
2022-03-09 12:38:27 -0500

    pet profile route updates

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -14,48 +14,43 @@ router.get('/signup', (req, res) => {
     res.render('signup');
 });
 
-router.get('/pet-profile', (req, res) => {
-  //get user including pet 
-    res.render('pet-profile');
-});
-
 router.get('/pet/:id', (req, res) => {
-  Pet.findOne({
-    where: {
-      id: req.params.id
-    },
-    attributes: [
-      "id",
-      "petName", 
-      "age",
-      "gender",
-      "breed",
-      "description"
-    ],
-    include: [
-      {
-        model: User,
-        attributes: ['username'],
-      }
-    ]
-  })
-    .then(dbPetData => {
-      if (!dbPetData) {
-        res.status(404),json({ messag: 'No pet found with this id'});
-        requestAnimationFrame;
-      }
+  // Pet.findOne({
+  //   where: {
+  //     id: req.params.id
+  //   },
+  //   attributes: [
+  //     "id",
+  //     "petName", 
+  //     "age",
+  //     "gender",
+  //     "breed",
+  //     "description"
+  //   ],
+  //   include: [
+  //     {
+  //       model: User,
+  //       attributes: ['username'],
+  //     }
+  //   ]
+  // })
+  //   .then(dbPetData => {
+  //     if (!dbPetData) {
+  //       res.status(404).json({ messag: 'No pet found with this id'});
+  //       requestAnimationFrame;
+  //     }
 
-      const pet = dbPetData.get({ plain: true});
+  //     const pet = dbPetData.get({ plain: true});
 
-      res.render('pet-profile', {
-        pet,
-        loggedIn: req.session.loggedIn
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    })
+  //     res.render('pet-profile', {
+  //       pet,
+  //       loggedIn: req.session.loggedIn
+  //     });
+  //   })
+  //   .catch(err => {
+  //     console.log(err);
+  //     res.status(500).json(err);
+  //   })
 });
 
 
---
+++ controllers/profile-routes.js
@@ -0,0 +1,37 @@
+const { Pet, User } = require('../models');
+
+const router = require('express').Router();
+
+//find all pets associate with user id
+router.get('/', (req, res) => {
+    console.log(req.session);
+    console.log('======================');
+    Pet.findAll({
+        where: {
+            owner_id: req.session.user_id
+        },
+        attributes: [
+            "id", 
+            "petName",
+            "age",
+            "breed",
+            "description"
+        ],
+        include: [
+            {
+                model: User,
+                attributes: ["username"]
+            }
+        ]
+    })
+    .then(dbPetData => {
+        const pets = dbPetData.map(pet=> pet.get({ plain: true}));
+        res.render('pet-profile', {pets, loggedIn: true});
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+})
+
+module.exports = router;
